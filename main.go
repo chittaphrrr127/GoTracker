377975c851bdbb7677d2d9299329d85af0003514
chittaphrrr <konigthehorse@gmail.com>
2026-09-05 18:10:30 +0500

    добавляю обработку ошибок

--- main.go
+++ main.go
@@ -1,43 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-type Order struct { //+
+type Order struct {
 	ID          int
 	Customer    string
 	Address     string
 	IsDelivered bool
 }
 
-type InMemoryOrderRepo struct { //+
-	orders map[int]Order
-}
-
-type OrderRepository interface { //+
-	Add(order Order)
-	GetByID(id int) (Order, bool)
-	Update(order Order)
-	GetAll() (order []Order)
-}
-
-func (o *Order) MarkDelivered() { //+
+func (o *Order) MarkDelivered() {
 	o.IsDelivered = true
 }
 
-func (i *InMemoryOrderRepo) Add(order Order) { //+
+var ErrOrderNotFound = errors.New("order not found")
+
+type OrderRepository interface {
+	Add(order Order)
+	GetByID(id int) (Order, error)
+	Update(order Order) error
+	GetAll() []Order
+}
+
+type InMemoryOrderRepo struct {
+	orders map[int]Order
+}
+
+func (i *InMemoryOrderRepo) Add(order Order) {
 	i.orders[order.ID] = order
 }
 
-func (i *InMemoryOrderRepo) GetByID(id int) (Order, bool) { //+
+func (i *InMemoryOrderRepo) GetByID(id int) (Order, error) {
 	order, ok := i.orders[id]
-	return order, ok
+	if !ok {
+		return Order{}, ErrOrderNotFound //valid zero-value Order. Not nil, because Order isn't a pointer/interface/map/etc.
+	}
+	return order, nil
 }
 
-func (i *InMemoryOrderRepo) Update(order Order) { //+
+func (i *InMemoryOrderRepo) Update(order Order) error { //
+	if _, ok := i.orders[order.ID]; !ok {
+		return ErrOrderNotFound
+	}
 	i.orders[order.ID] = order
+	return nil
 }
 
-func (i *InMemoryOrderRepo) GetAll() []Order { //+
+func (i *InMemoryOrderRepo) GetAll() []Order {
 	orders := []Order{}
 	for _, order := range i.orders {
 		orders = append(orders, order)
@@ -45,37 +57,29 @@ func (i *InMemoryOrderRepo) GetAll() []Order { //+
 	return orders
 }
 
-func PrintAllOrders(repo OrderRepository) { //+
+func PrintAllOrders(repo OrderRepository) {
 	orders := repo.GetAll()
 	for _, order := range orders {
-		fmt.Printf("ID: %d, %s, Delivered: %v\n", order.ID, order.Customer, order.IsDelivered)
+		fmt.Printf("ID: %d, %s, Delivered: %t\n", order.ID, order.Customer, order.IsDelivered)
 	}
 }
 
-func NewInMemoryOrderRepo() *InMemoryOrderRepo { //?
-	return &InMemoryOrderRepo{
+func main() { //
+	repo := InMemoryOrderRepo{
 		orders: make(map[int]Order),
 	}
-}
-
-func main() {
-	// repo := InMemoryOrderRepo{
-	// 	orders: make(map[int]Order),
-	// }
-
-	repo:=NewInMemoryOrderRepo()
 
 	order1 := Order{
 		ID:          1,
 		Customer:    "Andrey",
-		Address:     "A1",
+		Address:     "a",
 		IsDelivered: false,
 	}
 
 	order2 := Order{
 		ID:          2,
 		Customer:    "Ivan",
-		Address:     "B1",
+		Address:     "i",
 		IsDelivered: false,
 	}
 
@@ -83,14 +87,33 @@ func main() {
 	repo.Add(order2)
 
 	fmt.Println("[Before]")
-	PrintAllOrders(repo)
+	PrintAllOrders(&repo)
 
-	order, exists := repo.GetByID(1)
-	if exists {
+	id := 1
+
+	order, err := repo.GetByID(id)
+	if err != nil {
+		fmt.Println("Ошибка", err)
+	} else {
 		order.MarkDelivered()
-		repo.Update(order)
+		if err := repo.Update(order); err != nil {
+			fmt.Println("Ошибка:", err)
+		} else {
+			fmt.Printf("\nUpdated order %d\n", order.ID)
+		}
 	}
 
-	fmt.Println("[After]")
-	PrintAllOrders(repo)
+	fmt.Println("\n[After]")
+	PrintAllOrders(&repo)
+
+	order3 := Order{
+		ID:          3,
+		Customer:    "Assel",
+		Address:     "q",
+		IsDelivered: false,
+	}
+	erru := repo.Update(order3)
+	if erru != nil {
+		fmt.Println("\nОшибка:", erru)
+	}
 }
